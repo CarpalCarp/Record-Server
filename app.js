7c8057376e566f18c73d66f7949c7fc4d64ac596
Matias Galante <CarpalCarp@users.noreply.github.com>
2025-07-21 17:08:55 -0600

    Setup CORS since my requests from the frontend were getting denied.

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 const express = require('express');
+const cors = require('cors');
 const morgan = require('morgan');
 const recordRoutes = require('./routes/record_routes');
 
 const app = express();
+
+app.use(cors({
+  origin: ['http://localhost:4200']
+}));
 
 app.listen(3000);
 
